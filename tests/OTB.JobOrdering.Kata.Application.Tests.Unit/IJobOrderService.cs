4fa786ffdd83f69615ece10991c34fab732412a4
Zeeshan Tariq <zeeshan.tariq@vanquisbank.co.uk>
2021-04-21 07:35:31 +0100

    Red Stage - Added possible test cases. Tests failing, as no implementation done yet.

---
+++ tests/OTB.JobOrdering.Kata.Application.Tests.Unit/IJobOrderService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OTB.JobOrdering.Kata.Application.Tests.Unit
+{
+    internal interface IJobOrderService
+    {
+        IEnumerable<char> OrderJobs(string v);
+    }
+}
---
+++ tests/OTB.JobOrdering.Kata.Application.Tests.Unit/JobOrderService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace OTB.JobOrdering.Kata.Application.Tests.Unit
+{
+    internal class JobOrderService : IJobOrderService
+    {
+        public IEnumerable<char> OrderJobs(string v)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
